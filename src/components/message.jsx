d58d6632c8962f3f4111432cc2bfe98bba34c0da
Julien Da Silva <julien.da.silva@hotmail.com>
2017-09-27 11:13:24 +0200

    default message displayed

---
+++ src/components/message.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Message = () => {
+  return (
+    <div className="">
+      <p>MyName</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate asperiores</p>
+    </div>
+  );
+};
+
+export default Message;
---
+++ src/containers/message_list.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Message from '../components/message';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <Message />
+    );
+  }
+}
+
+export default MessageList;
