e0427b21118937c012d2c4f8f0bec4e381d1ec6b
Julien Da Silva <julien.da.silva@hotmail.com>
2017-09-27 10:51:22 +0200

    initial state with reducers

--- src/index.jsx
+++ src/index.jsx
@@ -8,9 +8,19 @@ import { createStore, combineReducers } from 'redux';
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+// Reducers
+import messagesReducer from './reducers/messages_reducer';
+import channelsReducer from './reducers/channels_reducer';
+import selectedChannelReducer from './reducers/selectedChannel_reducer';
+import currentUsernameReducer from './reducers/currentUsername_reducer';
+
 // State and reducers
 const reducers = combineReducers({
-  state: (state = {}, action) => state
+  // state: (state = {}, action) => state
+  messages: messagesReducer,
+  channels: channelsReducer,
+  selectedChannel: selectedChannelReducer,
+  currentUsername: currentUsernameReducer
 });
 
 // render an instance of the component in the DOM
